8d53db3b8faef62c5cbcf20ad0d77638116e12d6
magzupao <mguado@gmail.com>
2023-01-06 12:37:10 +0100

    modificamos el properties

--- src/main/java/com/refactorizando/kafka/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/refactorizando/kafka/kafka/consumer/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.refactorizando.kafka.kafka.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
