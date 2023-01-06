cf8bf79ca4dea899bc376eeb5b1fc3a61cd2de13
magzupao <mguado@gmail.com>
2023-01-06 12:30:34 +0100

    modificamos el properties

--- src/main/java/com/refactorizando/kafka/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/refactorizando/kafka/kafka/consumer/KafkaConsumer.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
--- src/main/java/com/refactorizando/kafka/kafka/producer/KafkaProducer.java
+++ src/main/java/com/refactorizando/kafka/kafka/producer/KafkaProducer.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
